2e9bbe90b681d22997741ca64776691e36bf20db
Nick Stocchero <nick123pig@gmail.com>
2016-09-17 18:48:12 -0400

    checkin

--- app/assets/javascripts/user_payment.js
+++
@@ -1,6 +0,0 @@
-
-$( document ).ready(function() {
-  $( "#amount" ).change(function() {
-    $("#target-button").data('amount', this.val()); 
-  });
-});
--- app/controllers/user_payments_controller.rb
+++ app/controllers/user_payments_controller.rb
@@ -9,40 +9,40 @@ class UserPaymentsController < ApplicationController
   def show
   end
 
+
   def create
     begin
-      @amount = cause_params[:amount]
+      @amount = params[:amount]
+
       @amount = @amount.gsub('$', '').gsub(',', '')
 
-      customer = Stripe::Customer.create(
-        :email => current_user.email,
-        :source  => params[:stripeToken]
-      )
+      begin
+        @amount = Float(@amount).round(2)
+      rescue
+        flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+        redirect_to new_charge_path
+        return
+      end
 
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => 'DC Grit Contribution',
-        :currency    => 'usd'
+      @amount = (@amount * 100).to_i # Must be an integer!
+
+      if @amount < 500
+        flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+        redirect_to new_charge_path
+        return
+      end
+
+      Stripe::Charge.create(
+        :amount => @amount,
+        :currency => 'usd',
+        :source => params[:stripeToken],
+        :description => 'Custom donation'
       )
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
     end
-
-    @user_payment = UserPayment.new(amount: @amount, cause: @cause, stripe_charge_id: charge.id)
-
-    respond_to do |format|
-      if @user_payment.save
-        format.html { redirect_to @user_payment, notice: 'You have successfully contributed!' }
-        format.json { render :show, status: :created, location: @user_payment }
-      else
-        format.html { render :new }
-        format.json { render json: @cause.errors, status: :unprocessable_entity }
-      end
-    end
-
   end
 
   private
@@ -57,7 +57,6 @@ class UserPaymentsController < ApplicationController
         redirect_to causes_path 
       end
     end
-
 
     def cause_params
       params.permit(:stripeToken, :cause_id, :amount)
