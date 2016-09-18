11d16644282e16627bf8737ae9126afea98708da
Nick Stocchero <nick123pig@gmail.com>
2016-09-18 12:40:46 -0400

    add description

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :location, :goal_amount)
+      params.require(:project).permit(:title, :location, :goal_amount, :description)
     end
 end
